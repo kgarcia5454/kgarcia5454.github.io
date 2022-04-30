393ba86976ab5aa90622d54410196ac4a4ee6b5c
kgarcia5454 <kgarcia5454@gmail.com>
2022-04-29 21:55:41 -0700

    Added icon and finally added text to About section
    Education has now been moved to the about section.
    Skills have been added to about section

--- JS/HomePage.js
+++ JS/HomePage.js
@@ -1,6 +1,5 @@
-
-
 var About = document.getElementById("About");
+
 
 var showCursor = false
 var speed = 750
@@ -17,17 +16,5 @@ setInterval(() => {
 },speed)
 
 function ShowAbout(){
-
-    if(About.style.height != "25vh"){   
-        About.style.height="25vh";
-        About.style.borderTop="3px ridge white";
-        About.style.borderBottom="3px ridge white";
-
-    }else{
-        About.style.height="0vh";
-        About.style.borderTop="0px ridge gray";
-        About.style.borderBottom="0px ridge gray";
-
-    }
-    
+    About.classList.toggle('open'); 
 }
